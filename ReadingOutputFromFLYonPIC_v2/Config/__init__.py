89b8898338aa682393e32db9a815f78bb3cd456f
BrianMarre <b.marre@hzdr.de>
2024-04-11 17:09:24 +0200

    fix copyright comment

--- ReadingOutputFromFLYonPIC_v2/Config/__init__.py
+++ ReadingOutputFromFLYonPIC_v2/Config/__init__.py
@@ -1,6 +1,6 @@
 """
 This file is part of the FLYonPIC_Eval.
-Copyright 2024 PIConGPU contributors
+Copyright 2024 FLYonPIC_Eval contributors
 
 Distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
