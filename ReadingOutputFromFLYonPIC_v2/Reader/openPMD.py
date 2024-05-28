192b8948f3300b2554614a1e833b97564088f201
BrianMarre <b.marre@hzdr.de>
2024-05-28 17:14:00 +0200

    adding read function for boundElectrons

--- ReadingOutputFromFLYonPIC_v2/Reader/openPMD.py
+++ ReadingOutputFromFLYonPIC_v2/Reader/openPMD.py
@@ -8,28 +8,30 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public 
 Authors: Brian Edward Marre
 License: GPLv3+
 """
-import openpmd_api as io
+import openpmd_api as opmd
 
 import numba
 import numpy as np
-import numpy.typing as npt
 from tqdm import tqdm
 
 import typeguard
 
 @numba.jit(fastmath=True, nopython=True, cache=True)
-def fastHistogram(weights, atomicStateCollectionIndices, typicalWeight, numberAtomicStates):
+def fastHistogram(weights, propertyIndex, typicalWeight, numberPropertyIndexValues):
     """ kernel for accumulating weight of entries according to their atomicStateCollectionIndex
 
         @param weights array of weight values
-        @param atomicStateCollectionIndices array of atomicStateCollectionIndices corresponding to weights
+        @param propertyIndex array of property index values corresponding to weights
         @param typicalWeight normalization factor for weight accumulation
-        @param numberAtomicStates number of atomic states in the input,
-            all entries in atomicStateCollectionIndices must be < numberAtomicStates
+        @param numberIndexValues number of unique property index values in the output
+
+        @attention 0 <= propertyIndex value < numberPropertyIndexValues
+
+        @returns accumulatedWeight[propertyIndex]
     """
 
     # number of possible atomicStateCollectionIndice Values
-    result = np.zeros(numberAtomicStates)
+    result = np.zeros(numberPropertyIndexValues)
 
     for entryIdx in range(atomicStateCollectionIndices.shape[0]):
         state = atomicStateCollectionIndices[entryIdx]
@@ -38,32 +40,35 @@ def fastHistogram(weights, atomicStateCollectionIndices, typicalWeight, numberAt
     return result
 
 @numba.jit(cache=True, parallel=True)
-def callFastHistogramInParallel(weights, atomicStates, typicalWeight, numberAtomicStates, numberWorkers, chunkSize):
+def callFastHistogramInParallel(weights, propertyIndex, typicalWeight, numberPropertyIndexValues, numberWorkers, chunkSize):
     """ stage for parallel call of fast histogram kernel
 
         @param weights array of weight values
-        @param atomicStates array of atomicStateCollectionIndices corresponding to weights
+        @param propertyIndex array of property index values corresponding to weights,
         @param typicalWeight normalization factor for weight accumulation
-        @param numberAtomicStates number of atomic states in the input,
-            all entries in atomicStateCollectionIndices must be < numberAtomicStates
+        @param numberIndexValues number of unique property index values in the output
         @param numberWorkers number of parallel worker to use
         @param chunkSize number of entries to attempt to pass to each worker each iteration
+
+        @attention 0 <= propertyIndex < numberPropertyIndexValues
+
+        @returns accumulatedWeight[numberPropertyIndexValues]
     """
 
     numberChunks = int(np.ceil(weights.shape[0] / chunkSize))
     numberIterations = int(np.ceil(numberChunks / numberWorkers))
 
-    result = np.zeros((numberWorkers + 1, numberAtomicStates), dtype=np.float32)
+    result = np.zeros((numberWorkers + 1, numberPropertyIndexValues), dtype=np.float32)
 
     for iterationIdx in range(numberIterations):
         for workerIdx in numba.prange(numberWorkers):
             if (numberChunks > (iterationIdx * numberWorkers + workerIdx)):
                 # get chunks of allocated memory, will return empty views if no data remaining
                 w_ = weights[(iterationIdx * numberWorkers + workerIdx) * chunkSize:(iterationIdx * numberWorkers + workerIdx + 1) * chunkSize]
-                s_ = atomicStates[(iterationIdx * numberWorkers + workerIdx) * chunkSize:(iterationIdx * numberWorkers + workerIdx + 1) * chunkSize]
+                p_ = propertyIndex[(iterationIdx * numberWorkers + workerIdx) * chunkSize:(iterationIdx * numberWorkers + workerIdx + 1) * chunkSize]
 
                 # store intermediate result in workers own memory
-                result[workerIdx + 1] += fastHistogram(w_, s_, typicalWeight, numberAtomicStates)
+                result[workerIdx + 1] += fastHistogram(w_, p_, typicalWeight, numberPropertyIndexValues)
 
         # sum worker results, to final accumulation
         result[0,:] = np.sum(result[1:], axis=0)
@@ -74,30 +79,31 @@ def callFastHistogramInParallel(weights, atomicStates, typicalWeight, numberAtom
     return result[0, :]
 
 @typeguard.typechecked
-def getAtomicPopulationData(
-        filename : str,
+def getPropertyIndexHistogram(
+        fileName : str,
         speciesName : str,
-        numberAtomicStates : int,
+        propertyName : str,
+        numberPropertyIndexValues : int,
         numberWorkers : int,
         chunkSize : int):
     """ returns the atomic state data of an openPMD particle output of a simulation
 
-        Paramters:
-            filename(string-like) ... filenames of data input
-            speciesName(string-like) ... string identifier of species
-            collectionIndex_to_atomicConfigNumber .. numpy.array[collectionIndex] = atomicConfigNumber as used in
-                FYLonPIC input
+        @param fileName absolute/relative path of output
+        @param speciesName string identifier of species
+        @param numberAtomicStates number of unique atomic states in output
+        @param numberWorkers number of independent threads to use
+        @param chunkSize number of particles to try to to pass each worker in a chunk
 
-        @returns np.array(numberIterations)= time and np.array((numberTimeSteps, numberAtomicStates))= accumulatedWeight
+        @returns np.array((numberTimeSteps, numberAtomicStates))= accumulatedWeight/scalingFactor, np.array(numberIterations)= time, scalingFactor
     """
-    series = io.Series(filename, io.Access.read_only)
+    series = opmd.Series(fileName, opmd.Access.read_only)
 
     numberIterations = np.shape(series.iterations)[0]
 
-    accumulatedWeight = np.empty((numberIterations, numberAtomicStates))
+    accumulatedWeight = np.empty((numberIterations, numberPropertyIndexValues))
     timeSteps = np.empty(numberIterations, dtype='f8')
 
-    print(filename + " iteration:")
+    print(fileName + " iteration:")
     for i, stepIdx in tqdm(enumerate(series.iterations)):
 
         # get subGroup for simulation iteration
@@ -107,12 +113,12 @@ def getAtomicPopulationData(
         species = step.particles[speciesName]
 
         # get recordComponents
-        weightingRecordComponent = species["weighting"][io.Mesh_Record_Component.SCALAR]
-        atomicStateCollectionIndexRecordComponent = species["atomicStateCollectionIndex"][io.Mesh_Record_Component.SCALAR]
+        weightingRecordComponent = species["weighting"][opmd.Mesh_Record_Component.SCALAR]
+        propertyIndexRecordComponent = species[propertyName][opmd.Mesh_Record_Component.SCALAR]
 
         # mark to be loaded, @todo give option to load chunkwise
         weights = weightingRecordComponent.load_chunk()
-        atomicStateCollectionIndices = atomicStateCollectionIndexRecordComponent.load_chunk()
+        propertyIndices = propertyIndexRecordComponent.load_chunk()
 
         # load data
         series.flush()
@@ -121,17 +127,69 @@ def getAtomicPopulationData(
 
         accumulatedWeight[i] = callFastHistogramInParallel(
             weights,
-            atomicStateCollectionIndices,
+            propertyIndices,
             typicalWeight,
-            numberAtomicStates,
+            numberPropertyIndexValues,
             numberWorkers,
             chunkSize)
         timeSteps[i] = step.get_attribute("time") * step.get_attribute("timeUnitSI")
 
         del weights
-        del atomicStateCollectionIndices
+        del propertyIndices
 
     # delete series
     del series
 
     return accumulatedWeight, timeSteps, typicalWeight
+
+@typeguard.typechecked
+def getAtomicPopulationData(
+        fileName : str,
+        speciesName : str,
+        numberAtomicStates : int,
+        numberWorkers : int,
+        chunkSize : int):
+    """ returns the atomic state data of an openPMD particle output of a simulation
+
+        @param fileName absolute/relative path of output
+        @param speciesName string identifier of species
+        @param numberAtomicStates number of unique atomic states in output
+        @param numberWorkers number of independent threads to use
+        @param chunkSize number of particles to try to to pass each worker in a chunk
+
+        @returns np.array((numberTimeSteps, numberAtomicStates))= accumulatedWeight/scalingFactor, np.array(numberIterations)= time, scalingFactor
+    """
+
+    return getPropertyIndexHistogram(
+        fileName,
+        speciesName,
+        "atomicStateCollectionIndex",
+        numberAtomicStates,
+        numberWorkers,
+        chunkSize)
+
+@typeguard.typechecked
+def getBoundElectronsData(
+    fileName : str,
+    speciesName : str,
+    numberChargeStates : int,
+    numberWorkers : int,
+    chunkSize : int):
+    """
+        returns the boundElectrons data of an atomic particle output of a simulation
+
+        @param fileName absolute/relative path of output
+        @param speciesName string identifier of species
+        @param numberAtomicStates number of unique charge states in output
+        @param numberWorkers number of independent threads to use
+        @param chunkSize number of particles to try to to pass each worker in a chunk
+
+        @returns np.array((numberTimeSteps, numberChargeStates))= accumulatedWeight/scalingFactor, np.array(numberIterations)= time, scalingFactor
+    """
+    return getPropertyIndexHistogram(
+        fileName,
+        speciesName,
+        "boundElectrons",
+        numberChargeStates,
+        numberWorkers,
+        chunkSize)
